bc1575b963c1317d2afac8e30ac9814773bf2d43
Zack Davis <jantrezac95@gmail.com>
2021-09-02 18:32:15 -0500

    server heroku update

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ const express = require("express");
 const fs = require("fs");
 
 let app = express();
-let PORT = process.env.PORT || 3001
+let PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/assets", express.static("./assets"));
+
+require("./routing/index")(app);
+require("./routing/html-notes")(app);
+
+app.listen(PORT, function() {
+    console.log("Listen on PORT " + PORT);
+});
