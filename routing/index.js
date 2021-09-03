ecf7f095808e47f582f47ede66c3e4ec5b7a44e0
Zack Davis <jantrezac95@gmail.com>
2021-09-02 21:59:36 -0500

    more heroku changes

--- routing/index.js
+++ routing/index.js
@@ -1,13 +1,14 @@
-const { NOTINITIALIZED } = require("dns");
 const fs = require("fs");
 let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app) {
-    app.get("/api/notes/:id", function(req, res) {
-        res.json(data[Number(req.params.id)]);
-    });
+    
     app.get("/api/notes", function(req, res) {
         res.json(data);
+    });
+    
+    app.get("/api/notes/:id", function(req, res) {
+        res.json(data[Number(req.params.id)]);
     });
 
     app.post("/api/notes", function(req, res) {
